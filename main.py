25d083f18c9f88a780d70b7afaa92367182c9c53
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-08 22:59:18 +0200

    solution

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     # write your code below this line
-
+    print('I will tell you a story, but I need some information first.')
+    name = input('What is the main character called?\n')
+    job = input('What is their job?\n')
+    print('Here is the story:')
+    print(f'Once upon a time there was {name}, who was a {job}')
+    print(f'On the way to work, {name} reflected on life.')
+    print(f'Perhaps {name} will not be a {job} forever.')
 
 if __name__ == '__main__':
     main()
